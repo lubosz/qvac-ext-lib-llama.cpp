3bb6c4ba8c9c5bdd6100f5553d2dd4308ea2886b
Jesús <jesus.mb.1995@gmail.com>
2025-07-16 11:07:22 +0200

    [common] Pure interface for files
    Convert llama_file to a pure virtual class that can be overriden by multiple implementations (disk, single memory buffer, ...)

--- src/llama-model-loader.cpp
+++ src/llama-model-loader.cpp
@@ -536,7 +536,7 @@ llama_model_loader::llama_model_loader(
     get_key(llm_kv(LLM_KV_GENERAL_ARCHITECTURE), arch_name, false);
     llm_kv = LLM_KV(llm_arch_from_string(arch_name));
 
-    files.emplace_back(new llama_file(fname.c_str(), "rb", use_direct_io));
+    files.emplace_back(new llama_file_disk(fname.c_str(), "rb", use_direct_io));
     contexts.emplace_back(ctx);
 
     if (use_mmap && use_direct_io) {
@@ -549,7 +549,7 @@ llama_model_loader::llama_model_loader(
 
             // reopen file using std::fopen for mmap
             files.pop_back();
-            files.emplace_back(new llama_file(fname.c_str(), "rb", false));
+            files.emplace_back(new llama_file_disk(fname.c_str(), "rb", false));
         }
     }
 
@@ -618,7 +618,7 @@ llama_model_loader::llama_model_loader(
                 }
             }
 
-            files.emplace_back(new llama_file(fname_split, "rb", use_direct_io));
+            files.emplace_back(new llama_file_disk(fname_split, "rb", use_direct_io));
             contexts.emplace_back(ctx);
 
             // Save tensors data offset info of the shard.
